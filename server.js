bc18223f4b19607567ccb4d5a985716aa0a9b458
Pranav <jacob.tj.tblr@gmail.com>
2021-04-15 18:05:16 +0530

    second commit

--- server.js
+++ server.js
@@ -5,10 +5,20 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:8080"
+    origin: "http://localhost:3000"
 };
 
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(function (req, res, next) {	
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');    
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');   
+  res.setHeader('Access-Control-Allow-Credentials', true);    
+  next();
+});
 
 
 // parse requests of content-type - application/json
